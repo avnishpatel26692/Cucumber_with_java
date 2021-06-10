4370521cddd0d6553f7d775fe44fa4670a50b878
Margarita Roze <margarita.roze@accenture.com>
2021-06-10 11:21:55 +0300

    Sample tried

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -46,6 +47,36 @@ public class SampleSteps {
     @And("^I should see menu$")
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+    }
+    @When("^user navigates to Age Page$")
+    public void user_navigates_to_Age_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age.html");
+    }
+
+    @When("^User enter name \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+        WebElement nameTextbox = driver.findElement(By.id("name"));
+        nameTextbox.clear();
+        nameTextbox.sendKeys(arg1);
+    }
+
+    @When("^User enter age (\\d+)$")
+    public void user_enter_age(int arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.name("age"));
+        ageText.sendKeys(""+arg1);
+    }
+
+    @When("^User clicks on submit button$")
+    public void user_clicks_on_submit_button() throws Throwable {
+        WebElement submitBtn = driver.findElement(By.xpath("//button[text()='Submit']"));
+        submitBtn.click();
+    }
+
+    @Then("^User see message \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        WebElement msg = driver.findElement(By.id("message"));
+        String atcualMessage = msg.getText();
+        Assert.assertEquals(arg1,atcualMessage );
     }
 }
 
---
+++ src/test/resources/features/Sample2.feature
@@ -0,0 +1,8 @@
+Feature: This is a feature to test Age Page functionality
+@scenario1
+Scenario: Check Age page functionality for Adult
+When user navigates to Age Page
+And User enter name "ABC"
+And User enter age 40
+And User clicks on submit button
+Then User see message "Hello, ABC, you are an adult"
