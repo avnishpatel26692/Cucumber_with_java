412806ca650c85647c20532a0f47bc80ac71eeea
Valery Stroikin <vstroiki@mail.ru>
2021-06-10 14:08:28 +0300

    Task1

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -47,6 +48,81 @@ public class SampleSteps {
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
     }
+
+    //////////////////////////////////////////////////////////
+
+    @When("^user navigates to Age Page$")
+    public void user_navigates_to_Age_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age.html");
+    }
+
+    @When("^User enter name \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+        WebElement nameTextbox = driver.findElement(By.id("name"));
+        nameTextbox.clear();
+        nameTextbox.sendKeys(arg1);
+    }
+
+    @When("^User enter age (\\d+)$")
+    public void user_enter_age(int arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.name("age"));
+        ageText.sendKeys(""+arg1);
+    }
+
+    @When("^User clicks on submit button$")
+    public void user_clicks_on_submit_button() throws Throwable {
+        WebElement submitBtn = driver.findElement(By.xpath("//button[text()='Submit']"));
+        submitBtn.click();
+    }
+
+    @Then("^User see message \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        WebElement msg = driver.findElement(By.id("message"));
+        String atcualMessage = msg.getText();
+        Assert.assertEquals(arg1,atcualMessage );
+    }
+
+//////////////////////////////////////////////
+
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+    }
+
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(int arg1) throws Throwable {
+        WebElement numberTextbox = driver.findElement(By.id("numb"));
+        //numberTextbox.clear();
+        numberTextbox.sendKeys(""+arg1);
+    }
+
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        WebElement submitButton = driver.findElement(By.xpath("//button[text()='Submit']"));
+        submitButton.click();
+    }
+
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+        WebElement msg = driver.findElement(By.id("message"));
+        String actualMessage = msg.getText();
+        Assert.assertEquals(arg1,actualMessage );
+    }
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg1) throws Throwable {
+        // Switching to Alert
+        Alert alert = driver.switchTo().alert();
+
+
+        // Capturing alert message.
+        String alertMessage= driver.switchTo().alert().getText();
+        Assert.assertEquals(arg1,alertMessage );
+
+        alert.accept();
+    }
+
+
 }
 
 
---
+++ src/test/resources/features/Sample3.feature
@@ -0,0 +1,23 @@
+@Feature3
+Feature: This is a feature to test Age Page functionality
+
+  @scenario1
+  Scenario Outline: Check Age page functionality for Adult
+    When user navigates to Age Page
+    And User enter name "<name>"
+    And User enter age <age>
+    And User clicks on submit button
+    Then User see message "<message>"
+    @Working
+    Examples:
+    | name | age | message|
+    | ABC | 40 | Hello, ABC, you are an adult |
+    | ABC | 5  | Hello, ABC, you are a kid    |
+    | ABC | 15 | Hello, ABC, you are a teenager|
+    | ABC | 45 | Hello, ABC, you are an adult|
+
+    @NotWorking
+    Examples:
+      | name | age | message|
+      | ABC | 45 | Hello, ABC, you are a kid|
+      | ABC | 45 | Hello, ABC, you are a teenager|
