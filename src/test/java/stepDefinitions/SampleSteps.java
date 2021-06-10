d564e88fd9f0014904d52bce1af48b5b460f0bf8
Arturs Afanasjevs <artur90@inbox.lv>
2021-06-10 11:17:10 +0300

    Sample2

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -25,29 +26,60 @@ public class SampleSteps {
     public SampleSteps() {
         this.driver = Hooks.driver;
     }
+//
+//    @Given("^I am on the home page$")
+//    public void iAmOnTheHomePage() throws Throwable {
+//        driver.get("https://kristinek.github.io/site/examples/age.html");
+//    }
+//
+//    @Then("^I should see home page header$")
+//    public void iShouldSeeHomePageHeader() throws Throwable {
+//        assertEquals("This is a home page",
+//                driver.findElement(By.cssSelector("h1")).getText());
+//    }
+//
+//    @And("^I should see home page description$")
+//    public void iShouldSeeHomePageDescription() throws Throwable {
+//        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//                driver.findElement(By.cssSelector("p")).getText());
+//    }
+//
+//    @And("^I should see menu$")
+//    public void iShouldSeeMenu() throws Throwable {
+//        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+//    }
+//}
 
-    @Given("^I am on the home page$")
-    public void iAmOnTheHomePage() throws Throwable {
-        driver.get("https://kristinek.github.io/site");
+    @When("^user navigates to Age Page$")
+    public void user_navigates_to_Age_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age.html");
     }
 
-    @Then("^I should see home page header$")
-    public void iShouldSeeHomePageHeader() throws Throwable {
-        assertEquals("This is a home page",
-                driver.findElement(By.cssSelector("h1")).getText());
+    @When("^User enter name \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+        WebElement nameTextbox = driver.findElement(By.id("name"));
+        nameTextbox.clear();
+        nameTextbox.sendKeys(arg1);
     }
 
-    @And("^I should see home page description$")
-    public void iShouldSeeHomePageDescription() throws Throwable {
-        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                driver.findElement(By.cssSelector("p")).getText());
+    @When("^User enter age (\\d+)$")
+    public void user_enter_age(int arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.name("age"));
+        ageText.sendKeys(""+arg1);
     }
 
-    @And("^I should see menu$")
-    public void iShouldSeeMenu() throws Throwable {
-        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+    @When("^User clicks on submit button$")
+    public void user_clicks_on_submit_button() throws Throwable {
+        WebElement submitBtn = driver.findElement(By.xpath("//button[text()='Submit']"));
+        submitBtn.click();
+    }
+
+    @Then("^User see message \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        WebElement msg = driver.findElement(By.id("message"));
+        String atcualMessage = msg.getText();
+        Assert.assertEquals(arg1,atcualMessage );
     }
 }
-
 
 
