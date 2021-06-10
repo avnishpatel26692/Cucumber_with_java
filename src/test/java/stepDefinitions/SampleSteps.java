d4244351dc65b59b1190321e0752dde113ef65a3
EduardNovickij <eduards.novickijs@gmail.com>
2021-06-10 13:53:40 +0300

    Added Sample3 and Task1 features

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -79,6 +79,37 @@ public class SampleSteps {
         WebElement message = driver.findElement(By.id("message"));
         Assert.assertEquals(arg1,message.getText());
     }
+
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+    }
+
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(String arg1) throws Throwable {
+        WebElement number = driver.findElement(By.id("numb"));
+        number.clear();
+        number.sendKeys(arg1);
+    }
+
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        WebElement button = driver.findElement(By.xpath("//button[@type='button']"));
+        button.click();
+    }
+
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+        WebElement message = driver.findElement(By.id("ch1_error"));
+        Assert.assertEquals(arg1,message.getText());
+    }
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg1) throws Throwable {
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals(arg1,alert.getText());
+        alert.dismiss();
+    }
 }
 
 
--- src/test/resources/features/Sample2.feature
+++ src/test/resources/features/Sample2.feature
@@ -1,9 +1,26 @@
+@Feature1
 Feature: Check Age page functionality
 
-@Sample2
+@Sample2.1
 Scenario: Check Age page functionality for Adult
 When User navigates to Age page
   And User enter name "ABC"
   And User enter age "40"
   And User clicks on submit button
 Then User see message "Hello, ABC, you are an adult"
+
+  @Sample2.2 @Regression
+  Scenario: Check Age page functionality for Kid
+    When User navigates to Age page
+    And User enter name "ABC"
+    And User enter age "1"
+    And User clicks on submit button
+    Then User see message "Hello, ABC, you are a kid"
+
+  @Sample2.2
+  Scenario: Check Age page functionality for Teenager
+    When User navigates to Age page
+    And User enter name "ABC"
+    And User enter age "16"
+    And User clicks on submit button
+    Then User see message "Hello, ABC, you are a teenager"
