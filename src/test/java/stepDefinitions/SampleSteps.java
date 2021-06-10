fa5e55da8034d8f8eb9276e3ede9664e3461f791
Irina Grigorjeva <mrs.igrigoryeva@gmail.com>
2021-06-10 14:19:29 +0300

    code, Irina Grigoryeva

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,34 +21,51 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertFalse;
 
 public class SampleSteps {
+
     private WebDriver driver;
 
     public SampleSteps() {
         this.driver = Hooks.driver;
     }
 
-    @Given("^I am on the home page$")
-    public void iAmOnTheHomePage() throws Throwable {
-        driver.get("https://kristinek.github.io/site");
+
+
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        driver.get ("https://kristinek.github.io/site/tasks/enter_a_number");
+
     }
 
-    @Then("^I should see home page header$")
-    public void iShouldSeeHomePageHeader() throws Throwable {
-        assertEquals("This is a home page",
-                driver.findElement(By.cssSelector("h1")).getText());
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(String arg1) throws Throwable {
+        WebElement fieldToEnterNumber = driver.findElement(By.id("numb"));
+        fieldToEnterNumber.sendKeys(arg1);
+
     }
 
-    @And("^I should see home page description$")
-    public void iShouldSeeHomePageDescription() throws Throwable {
-        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                driver.findElement(By.cssSelector("p")).getText());
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        WebElement fieldToEnterNumber2 = driver.findElement(By.className("w3-orange"));
+        fieldToEnterNumber2.click();
+
     }
 
-    @And("^I should see menu$")
-    public void iShouldSeeMenu() throws Throwable {
-        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+    WebElement errorMessage = driver.findElement(By.id(("ch1_error")));
+        String actualMessage = errorMessage.getText();
+        Assert.assertEquals(arg1, actualMessage);
     }
-}
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg1) throws Throwable {
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        String alertMsg = driver.switchTo().alert().getText();
+        Assert.assertEquals(arg1, alertMsg);
+    }
+    }
+
 
 
 
