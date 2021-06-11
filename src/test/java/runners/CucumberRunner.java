c591a07980593a339e0dcfebc573471e8bda9904
jekaterinamelsitova <katerinka21@inbox.lv>
2021-06-11 12:22:08 +0300

    SamplePO

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -8,11 +8,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/Sample5.feature",
+        features = "src/test/resources/features/SamplePO.feature",
         plugin = {"pretty", "html:cucumber-report/html-report",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
-        tags = {"@Scenario3"},
+        tags = {"@regression"},
          dryRun = false,
         glue = {"stepDefinitions"}
 )
