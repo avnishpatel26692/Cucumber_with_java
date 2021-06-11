0e053c48a66fbfb17a8731d30239f1f3b64362aa
jekaterinamelsitova <katerinka21@inbox.lv>
2021-06-11 10:55:41 +0300

    Sample5

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -21,45 +21,27 @@ public class SampleSteps {
     public SampleSteps() {
         this.driver = Hooks.driver;
     }
-    @Given("^I am on the home page$")
-    public void iAmOnTheHomePage() throws Throwable {
-        driver.get("https://kristinek.github.io/site");
-    }
-    @Then("^I should see home page header$")
-    public void iShouldSeeHomePageHeader() throws Throwable {
-        assertEquals("This is a home page",
-                driver.findElement(By.cssSelector("h1")).getText());
-    }
-    @And("^I should see home page description$")
-    public void iShouldSeeHomePageDescription() throws Throwable {
-        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                driver.findElement(By.cssSelector("p")).getText());
-    }
-    @And("^I should see menu$")
-    public void iShouldSeeMenu() throws Throwable {
-        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
 
-    }
 
-    @When("^user navigates to Age Page$")
-    public void user_navigates_to_Age_Page() throws Throwable {
-        driver.get("https://kristinek.github.io/site/examples/age.html");
-    }
+@Given("^user navigates to Age Page$")
+public void user_navigates_to_Age_Page() throws Throwable {
+    driver.get("https://kristinek.github.io/site/examples/age.html");
+}
 
-    @When("^User enter name \"([^\"]*)\"$")
+    @Then("^user enter name \"([^\"]*)\"$")
     public void user_enter_name(String arg1) throws Throwable {
         WebElement nameTextbox = driver.findElement(By.id("name"));
         nameTextbox.clear();
         nameTextbox.sendKeys(arg1);
     }
 
-    @When("^User enter age (\\d+)$")
+    @And("^user enter age \"([^\"]*)\"$")
     public void user_enter_age(int arg1) throws Throwable {
         WebElement ageText = driver.findElement(By.name("age"));
-        ageText.sendKeys(""+arg1);
+        ageText.sendKeys("" + arg1);
     }
 
-    @When("^User clicks on submit button$")
+    @And("^User clicks on submit button$")
     public void user_clicks_on_submit_button() throws Throwable {
         WebElement submitBtn = driver.findElement(By.xpath("//button[text()='Submit']"));
         submitBtn.click();
@@ -68,50 +50,8 @@ public class SampleSteps {
     @Then("^User see message \"([^\"]*)\"$")
     public void user_see_message(String arg1) throws Throwable {
         WebElement msg = driver.findElement(By.id("message"));
-        String atcualMessage = msg.getText();
-        Assert.assertEquals(arg1,atcualMessage );
-    }
-
-
-    @Given("^I am on number page$")
-    public void i_am_on_number_page() throws Throwable {
-        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
-    }
-
-    @When("^I enter number: \"([^\"]*)\"$")
-    public void i_enter_number(int arg1) throws Throwable {
-        WebElement numberTextbox = driver.findElement(By.id("numb"));
-        //numberTextbox.clear();
-        numberTextbox.sendKeys(""+arg1);
-    }
-
-    @When("^I click submit number$")
-    public void i_click_submit_number() throws Throwable {
-        WebElement submitButton = driver.findElement(By.xpath("//button[text()='Submit']"));
-        submitButton.click();
-    }
-
-    @Then("^I see a message: \"([^\"]*)\"$")
-    public void i_see_a_message(String arg1) throws Throwable {
-        WebElement msg = driver.findElement(By.id("message"));
         String actualMessage = msg.getText();
-        Assert.assertEquals(arg1,actualMessage );
+        Assert.assertEquals(arg1, actualMessage);
     }
-
-    @Then("^I see a new message: \"([^\"]*)\"$")
-    public void i_see_a_new_message(String arg1) throws Throwable {
-        // Switching to Alert
-        Alert alert = driver.switchTo().alert();
-
-
-        // Capturing alert message.
-        String alertMessage= driver.switchTo().alert().getText();
-        Assert.assertEquals(arg1,alertMessage );
-
-        alert.accept();
-    }
-
-
 }
-
 
--- src/test/java/stepDefinitions/SampleSteps4.java
+++ src/test/java/stepDefinitions/SampleSteps4.java
@@ -1,5 +1,4 @@
 package stepDefinitions;
-
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -10,20 +9,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.Map;
 
 
 public class SampleSteps4 {
     private WebDriver driver;
-
     public SampleSteps4() {
         this.driver = Hooks.driver;
     }
-
     @Given("^user navigates to action page$")
     public void user_navigates_to_action_page() throws Throwable {
         driver.get("https://kristinek.github.io/site/examples/actions");
     }
-
     @When("^user clicks on checkboxes$")
     public void user_clicks_on_checkboxes(List<String> arg1) throws Throwable {
         for(String checkBoxValue : arg1)
@@ -33,13 +30,11 @@ public class SampleSteps4 {
             checkbox.click();
         }
     }
-
     @When("^user clicked on result checkbox button$")
     public void user_clicked_on_result_checkbox_button() throws Throwable {
         WebElement submitBtn = driver.findElement(By.id("result_button_checkbox"));
         submitBtn.click();
     }
-
     @Then("^message should be displayed \"([^\"]*)\"$")
     public void message_should_be_displayed(String arg1) throws Throwable {
         WebElement msg = driver.findElement(By.id("result_checkbox"));
@@ -47,6 +42,19 @@ public class SampleSteps4 {
         Assert.assertEquals(arg1,actualMsg );
     }
 
+    @When("^user enters details$")
+    public void user_enters_details(Map<String, String> arg1) throws Throwable {
+        for(Map.Entry<String, String> txtbox: arg1.entrySet())
+        {
+            String key = txtbox.getKey(); //name
+            String value = txtbox.getValue(); //ABC
+            WebElement textbox = driver.findElement(By.id(key));
+            textbox.clear();
+            textbox.sendKeys(value);
+        }
+
+    }
 }
+
 
 
