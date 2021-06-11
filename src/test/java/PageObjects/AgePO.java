58f1a37754afad9ef7d5673361620af8cbb4aab1
Margarita Roze <margarita.roze@accenture.com>
2021-06-11 17:16:45 +0300

    AgePO/AgeSubmittedPO/SamplePO updated

---
+++ src/test/java/PageObjects/AgePO.java
@@ -0,0 +1,33 @@
+package PageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class AgePO {
+    @FindBy(how = How.ID, using = "name")
+    private WebElement nameTxtbox;
+
+    @FindBy(how = How.ID, using = "age")
+    private WebElement ageTxtbox;
+
+    @FindBy(how = How.ID, using = "submit")
+    private WebElement submitBtn;
+
+    public void setName(String name)
+    {
+        nameTxtbox.clear();
+        nameTxtbox.sendKeys(name);
+    }
+
+    public void setAge(String age)
+    {
+        ageTxtbox.sendKeys(age);
+    }
+
+    public void clickOnSubmit()
+    {
+        submitBtn.click();
+    }
+
+}
