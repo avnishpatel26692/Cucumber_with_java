b64c0ae3627cecb67abe563dc22bc454a169fb2a
Arturs Afanasjevs <artur90@inbox.lv>
2021-06-11 18:17:38 +0300

    Task2

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -26,61 +27,62 @@ public class SampleSteps {
     public SampleSteps() {
         this.driver = Hooks.driver;
     }
-//
-//    @Given("^I am on the home page$")
-//    public void iAmOnTheHomePage() throws Throwable {
-//        driver.get("https://kristinek.github.io/site/examples/age.html");
-//    }
-//
-//    @Then("^I should see home page header$")
-//    public void iShouldSeeHomePageHeader() throws Throwable {
-//        assertEquals("This is a home page",
-//                driver.findElement(By.cssSelector("h1")).getText());
-//    }
-//
-//    @And("^I should see home page description$")
-//    public void iShouldSeeHomePageDescription() throws Throwable {
-//        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-//                driver.findElement(By.cssSelector("p")).getText());
-//    }
-//
-//    @And("^I should see menu$")
-//    public void iShouldSeeMenu() throws Throwable {
-//        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
-//    }
-//}
+
+    @Given("^I am on the home page$")
+    public void iAmOnTheHomePage() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age.html");
+    }
+
+    @Then("^I should see home page header$")
+    public void iShouldSeeHomePageHeader() throws Throwable {
+        assertEquals("This is a home page",
+                driver.findElement(By.cssSelector("h1")).getText());
+    }
+
+    @And("^I should see home page description$")
+    public void iShouldSeeHomePageDescription() throws Throwable {
+        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                driver.findElement(By.cssSelector("p")).getText());
+    }
+
+    @And("^I should see menu$")
+    public void iShouldSeeMenu() throws Throwable {
+        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+    }
+
 
 //    @When("^user navigates to Age Page$")
 //    public void user_navigates_to_Age_Page() throws Throwable {
 //        driver.get("https://kristinek.github.io/site/examples/age.html");
 //    }
-//
-//    @When("^User enter name \"([^\"]*)\"$")
-//    public void user_enter_name(String arg1) throws Throwable {
-//        WebElement nameTextbox = driver.findElement(By.id("name"));
-//        nameTextbox.clear();
-//        nameTextbox.sendKeys(arg1);
-//    }
-//
-//    @When("^User enter age (\\d+)$")
-//    public void user_enter_age(int arg1) throws Throwable {
-//        WebElement ageText = driver.findElement(By.name("age"));
-//        ageText.sendKeys(""+arg1);
-//    }
-//
-//    @When("^User clicks on submit button$")
-//    public void user_clicks_on_submit_button() throws Throwable {
-//        WebElement submitBtn = driver.findElement(By.xpath("//button[text()='Submit']"));
-//        submitBtn.click();
-//    }
 
-//    @Then("^User see message \"([^\"]*)\"$")
-//    public void user_see_message(String arg1) throws Throwable {
-//        WebElement msg = driver.findElement(By.id("message"));
-//        String atcualMessage = msg.getText();
-//        Assert.assertEquals(arg1,atcualMessage );
-//    }
-//}
+    @When("^User enter name \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+        WebElement nameTextbox = driver.findElement(By.id("name"));
+        nameTextbox.clear();
+        nameTextbox.sendKeys(arg1);
+    }
+
+    @When("^User enter age (\\d+)$")
+    public void user_enter_age(int arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.name("age"));
+        ageText.sendKeys("" + arg1);
+    }
+
+    @When("^User clicks on submit button$")
+    public void user_click_on_submit_button() throws Throwable {
+        WebElement submitBtn = driver.findElement(By.id("submit"));
+        submitBtn.click();
+    }
+
+    //
+    @Then("^User see message \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        WebElement msg = driver.findElement(By.id("message"));
+        String atcualMessage = msg.getText();
+        Assert.assertEquals(arg1, atcualMessage);
+    }
+
 
     @Given("^I am on number page$")
     public void IamOnNumberPage() throws Throwable {
@@ -97,7 +99,7 @@ public class SampleSteps {
 
     @When("^I click submit number$")
     public void user_clicks_on_submit_button() throws Throwable {
-        WebElement submitBtn = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        WebElement submitBtn = driver.findElement(By.id("submit"));
         submitBtn.click();
     }
 
@@ -112,10 +114,28 @@ public class SampleSteps {
     public void I_see_a_new_message(String arg1) throws Throwable {
         Alert alert = driver.switchTo().alert();
         String alertMsg = driver.switchTo().alert().getText();
-        Assert.assertEquals(arg1,alertMsg);
+        Assert.assertEquals(arg1, alertMsg);
     }
 
+    @Given("^I have the following order$")
+    public void i_have_the_following_order(DataTable arg1) throws Throwable {
+        for (Map<String, String> map : arg1.asMaps(String.class, String.class)) {
+            String vegetableName = map.get("vegetable"); //cucumber
+            String quantity = map.get("quantity"); // 4
+            String cost = map.get("cost"); //10
+            String availability = map.get("Availability"); // yes
+            System.out.println("========================");
+            System.out.println("Vegetable: " + vegetableName);
+            System.out.println("Quantity: " + quantity);
+            System.out.println("Cost: " + cost);
+            System.out.println("Availability: " + availability);
+            System.out.println("========================");
+        }
+    }
 
 }
+
+
+
 
 
