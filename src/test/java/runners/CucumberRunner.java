42e4551a4f13b3d89d55001c10a25518a17d2b34
jekaterinamelsitova <katerinka21@inbox.lv>
2021-06-11 10:15:16 +0300

    Day2

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -8,12 +8,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/Task1.feature",
+        features = "src/test/resources/features/Sample4.feature",
         plugin = {"pretty", "html:cucumber-report/html-report",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
-        tags = {"@Scenario1, @Scenario2"},
-        dryRun = false,
+        //tags = {"@Scenario1"},
+         dryRun = false,
         glue = {"stepDefinitions"}
 )
 public class CucumberRunner {
