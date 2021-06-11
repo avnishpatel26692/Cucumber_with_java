09fac15126ea2e03a39da6279e324fc8f6dad3a8
Margarita Roze <margarita.roze@accenture.com>
2021-06-11 17:07:48 +0300

    Task2Feature/SampleSteps4/CucumberRunner updated

--- src/test/java/stepDefinitions/SampleSteps4.java
+++ src/test/java/stepDefinitions/SampleSteps4.java
@@ -6,9 +6,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -75,7 +81,143 @@ public class SampleSteps4 {
                 System.out.println("========================");
             }
         }
+    @Given("^I am on Employee page$")
+    public void i_am_on_Employee_page() throws Throwable {
+       driver.get("https://kristinek.github.io/site/tasks/list_of_people");
 
     }
+
+    @When("^click on Add person$")
+    public void click_on_Add_person() throws Throwable {
+       WebElement AddPersn = driver.findElement(By.id("addPersonBtn"));
+        AddPersn.click();
+    }
+
+    @When("^fill all fields: \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\"$")
+    public void fill_all_fields(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7) throws Throwable {
+      WebElement nameTextbox = driver.findElement(By.id("name"));
+      nameTextbox.sendKeys(arg1);
+      WebElement SurnTextbox = driver.findElement(By.id("surname"));
+      SurnTextbox.sendKeys(arg2);
+      WebElement JobTextbox = driver.findElement(By.id("job"));
+      JobTextbox.sendKeys(arg3);
+      WebElement DateTextbox = driver.findElement(By.id("dob"));
+      DateTextbox.sendKeys(arg4+ Keys.TAB);
+      WebElement CheckBox = driver.findElement(By.id(arg5));
+      if(!CheckBox.isSelected()){
+          CheckBox.click();}
+
+          WebElement radioBtn1 = driver.findElement(By.id(arg6));
+          radioBtn1.click();
+        WebElement dropDownBox = driver.findElement(By.id("status"));
+        Select dropdownSelect = new Select(dropDownBox);
+        dropdownSelect.selectByVisibleText(arg7);
+
+      }
+
+
+
+    @Then("^click button add$")
+    public void click_button_add() throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("modal_button")));
+    WebElement AddBtn = driver.findElement(By.id("modal_button"));
+    AddBtn.click();
+    }
+
+    @When("^click on edit button$")
+    public void click_on_edit_button1() throws Throwable {
+        WebElement EditBtn = driver.findElement(By.xpath("//span[contains(@onclick,'openModalForEditPerson(3)')]"));
+        EditBtn.click();
+
+    }
+
+    @When("^edit something: \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\"$")
+    public void edit_something(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7) throws Throwable {
+        WebElement nameTextbox = driver.findElement(By.id("name"));
+        nameTextbox.clear();
+        nameTextbox.sendKeys(arg1);
+        WebElement SurnTextbox = driver.findElement(By.id("surname"));
+        SurnTextbox.clear();
+        SurnTextbox.sendKeys(arg2);
+        WebElement JobTextbox = driver.findElement(By.id("job"));
+        JobTextbox.clear();
+        JobTextbox.sendKeys(arg3);
+        WebElement DateTextbox = driver.findElement(By.id("dob"));
+        DateTextbox.clear();
+        DateTextbox.sendKeys(arg4+Keys.TAB);
+        WebElement CheckBox = driver.findElement(By.id(arg5));
+        if(!CheckBox.isSelected()){
+            CheckBox.click();}
+
+        WebElement radioBtn1 = driver.findElement(By.id(arg6));
+        radioBtn1.click();
+        WebElement dropDownBox = driver.findElement(By.id("status"));
+        Select dropdownSelect = new Select(dropDownBox);
+        dropdownSelect.selectByVisibleText(arg7);
+
+    }
+
+    @When("^click on edit-button$")
+    public void click_on_edit_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement editBtn = driver.findElement(By.id("modal_button"));
+        editBtn.click();
+    }
+
+    @When("^remove a person$")
+    public void remove_a_person() throws Throwable {
+        WebElement RemoveBtn = driver.findElement(By.xpath("//span[@onclick='deletePerson(3)']"));
+        RemoveBtn.click();
+
+    }
+
+    @Then("^reset list$")
+    public void reset_list() throws Throwable {
+        WebElement ResetBtn = driver.findElement(By.xpath("//button[text()='Reset List']"));
+        ResetBtn.click();
+
+    }
+
+    @When("^click clear-button$")
+    public void click_clear_button() throws Throwable {
+        WebElement ClearBtn = driver.findElement(By.xpath("//button[text()='Clear all fields']"));
+        ClearBtn.click();
+
+
+    }
+
+    @Then("^check empty fields$")
+    public void check_empty_fields() throws Throwable {
+        WebElement nameTextbox = driver.findElement(By.id("name"));
+       Assert.assertTrue(nameTextbox.getText().isEmpty());
+        WebElement SurnTextbox = driver.findElement(By.id("surname"));
+        Assert.assertTrue(SurnTextbox.getText().isEmpty());
+        WebElement JobTextbox = driver.findElement(By.id("job"));
+        Assert.assertTrue(JobTextbox.getText().isEmpty());
+        WebElement DateTextbox = driver.findElement(By.id("dob"));
+        Assert.assertTrue(DateTextbox.getText().isEmpty());
+       List<WebElement> checkboxes = driver.findElements(By.className("w3-check"));
+        for(WebElement elementName : checkboxes)
+        {
+            if (elementName.getAttribute("id").equals("english"))
+                Assert.assertTrue(elementName.isSelected());
+            else
+                Assert.assertFalse(elementName.isSelected());
+        }
+
+       List<WebElement> radioBtns = driver.findElements(By.className("w3-radio"));
+      for (WebElement radioBtn : radioBtns) {
+          Assert.assertFalse(radioBtn.isSelected());
+          WebElement DropDown = driver.findElement(By.id("status"));
+          Select DropDown1 = new Select(DropDown);
+          Assert.assertEquals("Employee", DropDown1.getFirstSelectedOption().getText());
+      }}}
+
+
+
+
+
+
     
 
